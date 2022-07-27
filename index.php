d8a89bc65ebc049f70ba5210f71330feff0add04
Dmusharo <dmusharo@gmail.com>
2022-07-27 18:05:53 +0300

    Lesson 3 task 2

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
-<meta charset="utf-8"
+<meta charset="utf-8">
+//Вывод русского  текста корректно
 <?php
 // echo "Hello Git";
-//lesson 3.1
-error_reporting(E_ALL);
-ini_set("display_errors", "on");
-mb_internal_encoding("UTF-8");
-
+//lesson 3 task 1
+error_reporting(E_ALL); //вывод ошибок в браузере. Notice -проверить код и устранить Warning-запуститься но может не так работать   Fatal-PHP не может быть выполнен. Проверить синтаксис программы
+ini_set("display_errors", "on"); // Если не работает первая. Разница в настройках сервера
+mb_internal_encoding("UTF-8"); //Вывод русского  текста корректно
+// errorr_reporting(0);  что бы сайт работал и ошибки не показывались
 include "Code.php";
+
+//lesson 3 task 2
+var_dump('12345'); // 'выведет 12345' и тип выводимых данных
+/*
+ * В круглых скобках пишем параметры функции. Несколько параметров пишем через запятую.
+ */
+
+
 ?>
 
 
