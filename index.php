0770272cd0bb6d362a2522ee5f4c3a0a1ed60526
Dmusharo <dmusharo@gmail.com>
2022-07-28 11:28:06 +0300

    Lesson 3. Task 19

--- index.php
+++ index.php
@@ -2,35 +2,56 @@
 //Вывод русского  текста корректно
 <?php
 
-//Lesson 3. Task 18
-// Сокращенные операции
-
+//Lesson 3. Task 19
+// Операции инкремента и декремента
+// оператор ++  это прибавление единицы -- отбавление 1
 //1
-$num=1;
-$num=$num+1;
-$num=$num+1;
+$num=10;
+$num++;
+$num++;
+$num--;
 echo $num;
-
-$num+=3; //$num=$num+3
-$num-=3; //$num=$num-3
-$num*=3; //$num=$num*3
-$num/=3; //$num=$num/3
-
-$str="a";
-$str.="b"; //$str=$str."b";
+//Префиксный и постпрефиксный тип $a++ and ++$a;
+//2
+$num = 3;
+echo ++$num; //выведет 4
 
 //3
-$num=47;
-$num+=7;
-$num-=18;
-$num*=10;
-$num/=15;
-echo $num;
+$num=3;
+echo $num++; //выведет 3
 //4
-$str='a';
-$str.='b';
-$str.='c';
-echo $str;
+$num=3;
+echo --$num; // выведет 2
+//5
+$num=3;
+echo $num--; // выведет 3
+//6
+$num1=3;
+$num2=++$num1;
+echo $num1; //4
+echo $num2; //4
+//7
+$num1=3;
+$num2=$num1++;
+echo $num1; //4 Повторить
+echo $num2; //3
+//8
+$num1=3;
+$num2=--$num1;
+echo $num1; // 2
+echo $num2; // 2
+//9
+$num1=3;
+$num2=$num1--;
+echo $num1;//2
+echo $num2;//3
+//10
+$num1=3; //3
+$num1++; //4
+$num2=$num1--;
+echo $num1++; //3
+echo --$num2; //3
+
 
 ?>
 
