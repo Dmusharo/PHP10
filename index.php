8a72a639815394d2bfe4afe84db1dd3993e8230d
Dmusharo <dmusharo@gmail.com>
2022-07-28 15:30:48 +0300

    Lesson 4. Task 23

--- index.php
+++ index.php
@@ -2,23 +2,22 @@
 //Вывод русского  текста корректно
 <?php
 
-//Lesson 4. Task 22
-// Массивы
-//1-2
-$arr=['a','b','c']; // создание массива
-echo $arr[0],$arr[1],$arr[2];
-//echo $arr;
+//Lesson 4. Task 23
+// Ассоциативные Массивы
+//1
+$arr=[1=>'a',2=>'b',3=>'c'];
+var_dump($arr);
+//2
+$arr=[1=>'январь','февраль',"март","апрель","май","июнь","июль"];
 var_dump($arr);
 //3
-$arr=['a','b','c','d']; // создание массива
-echo $arr[0],'+',$arr[1],'+',$arr[2],'+',$arr[3];
+$arr=['name'=> 'denys','surname'=>'Musharov','patromymic'=>"victorovich"];
+var_dump($arr);
+echo $arr['surname'],' ', $arr['name'],' ',$arr['patromymic'];
+
 //4
-$arr=[1,2,3];
-echo $arr[0]+$arr[1]+$arr[2];
-//5
-$arr=[2,5,3,9];
-$result=$arr[0]*$arr[1]+$arr[2]*$arr[3];
-echo $result;
+$data=['year'=>2022,'month'=>07,'day'=>28];
+var_dump($data);
 ?>
 
 
