dde7cb45892d65fb94c790c7bcf963c0e5644c81
Dmusharo <dmusharo@gmail.com>
2022-07-28 10:37:44 +0300

    Lesson 3. Task 18

--- index.php
+++ index.php
@@ -1,43 +1,36 @@
-<meta charset="utf-8">
+// <meta charset="utf-8">
 //Вывод русского  текста корректно
 <?php
 
-//Lesson 3. Task 17
-// Получение символов строки
+//Lesson 3. Task 18
+// Сокращенные операции
+
 //1
-$str='abcde';
-echo $str[0],$str[2],$str[4];
-//2
-$str='abcde';
-echo $str[4],$str[3],$str[2],$str[1],$str[0];
+$num=1;
+$num=$num+1;
+$num=$num+1;
+echo $num;
+
+$num+=3; //$num=$num+3
+$num-=3; //$num=$num-3
+$num*=3; //$num=$num*3
+$num/=3; //$num=$num/3
+
+$str="a";
+$str.="b"; //$str=$str."b";
+
 //3
-$str='abcde';
-$num=3;
-echo $str[$num];
-//4 strlen  Функция для нахождения длины строки
-
-$str='abcdeddl';
-echo $str[strlen($str)-1];
-//5
-$str='abcdeddl';
-echo $str[strlen($str)-2];
-//6
-$str='abcdeddl';
-echo $str[strlen($str)-3];
-
-//7
-$str="12345";
-echo '$str[0]+$str[1]+$str[2]+$str[3]+$str[4]';
-//8
-$a=12345;
-$str=(string)$a;
-echo $str[0]+$str[1]+$str[2]+$str[3]+$str[4];
-//9
-echo $str[0]*$str[1]*$str[2]*$str[3]*$str[4];
-//10
-$a=12345;
-$str=(string)$a;
-echo $str[4],$str[3],$str[2],$str[1],$str[0];
+$num=47;
+$num+=7;
+$num-=18;
+$num*=10;
+$num/=15;
+echo $num;
+//4
+$str='a';
+$str.='b';
+$str.='c';
+echo $str;
 
 ?>
 
