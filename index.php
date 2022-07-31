bd04debc3d2786ef1dcbc7ad8dbe482f0faf81f4
Dmusharo <dmusharo@gmail.com>
2022-07-31 22:54:43 +0300

    Lesson 4. Task 35

--- index.php
+++ index.php
@@ -1,60 +1,103 @@
 <?php
-//Lesson 4. Task 34
-//   Команда isset               !==
-//Подавление аорнинга  isset  без ворнинга
-//инвертирование высказывания
-// Элементы массива
+//Lesson 4. Task 35
+//   Команда empty
+//  это 0  "0" false null
 //1
 $test=0;
-if (isset($test)) {
-    echo '+'; //null
+if (empty($test)) {
+    echo '+'; //выведет +
 } else {
     echo '-';
 }
 //2
-$test=null;
-if (!isset($test)) {
-    echo '+'; // выведет +
+$test=-1;
+if (empty($test)) {
+    echo '+';
 } else {
-    echo '-';
+    echo '-';//выведет -
 }
 //3
-$test=null;
-if (isset($test)) {
-    echo '+'; //
+$test='';
+if (!empty($test)) {
+    echo '+';
 } else {
-    echo '-';// выведет -
+    echo '-';//выведет -
 }
 //4
-if (!isset($test)) {
-    echo '+'; // выведет +
+$test=-1;
+if (!empty($test)) {
+    echo '+'; //выведет +
 } else {
     echo '-';
 }
 //5
-$test='';
-if (isset($test)) {
-    echo '+'; //
+$test="0";
+if (!empty($test)) {
+    echo '+';
 } else {
-    echo '-';
+    echo '-';//выведет -
 }
+
 //6
-if (!isset($test)) {
-    echo '+'; //выведет +
+$test=-1;
+if (!empty($test)) {
+    echo '+';//выведет +
 } else {
     echo '-';
 }
 //7
-$test=false;
-if (isset($test)) {
-    echo '+'; //выведет +
+$test=null;
+if (empty($test)) {
+    echo '+';//выведет +
 } else {
     echo '-';
 }
 //8
-$arr = [1, 2, 3, 4, 5];
-if (isset($arr)) {
+$test=false;
+if (!empty($test)) {
     echo '+';
+} else {
+    echo '-';//выведет -
+}
+//9
+$test=true;
+if (!empty($test)) {
+    echo '+';//выведет +
+} else {
+    echo '-';
+}
+//10
+$test=false;
+if (!empty($test)) {
+    echo '+';
+} else {
+    echo '-';//выведет -
+}
+//11
+$test=null;
+if (!empty($test)) {
+    echo '+';
+} else {
+    echo '-';//выведет -
+}
+//12
+$test=0;
+if (isset($test)) {
+    echo '+';//выведет +
+} else {
+    echo '-';
+}
+//13
+$test=null;
+if (isset($test)) {
+    echo '+';//выведет -
+} else {
+    echo '-';
+}
+//14
+$test=false;
+if (isset($test)) {
+    echo '+';//выведет +
 } else {
     echo '-';
 }
