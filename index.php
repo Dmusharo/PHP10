3e5b1d1840b253caf693276b93c931839ed44f56
Dmusharo <dmusharo@gmail.com>
2022-07-31 23:15:17 +0300

    Lesson 4. Task 36

--- index.php
+++ index.php
@@ -1,106 +1,18 @@
 <?php
-//Lesson 4. Task 35
-//   Команда empty
-//  это 0  "0" false null
+//Lesson 4. Task 36
+//  Сокращенный синтаксис if else не является обязательной.
 //1
-$test=0;
-if (empty($test)) {
-    echo '+'; //выведет +
-} else {
-    echo '-';
+$test=10;
+if ($test==10){
+    echo"Верно";
 }
 //2
-$test=-1;
-if (empty($test)) {
-    echo '+';
-} else {
-    echo '-';//выведет -
-}
-//3
-$test='';
-if (!empty($test)) {
-    echo '+';
-} else {
-    echo '-';//выведет -
-}
-//4
-$test=-1;
-if (!empty($test)) {
-    echo '+'; //выведет +
-} else {
-    echo '-';
-}
-//5
-$test="0";
-if (!empty($test)) {
-    echo '+';
-} else {
-    echo '-';//выведет -
-}
-
-//6
-$test=-1;
-if (!empty($test)) {
-    echo '+';//выведет +
-} else {
-    echo '-';
-}
-//7
-$test=null;
-if (empty($test)) {
-    echo '+';//выведет +
-} else {
-    echo '-';
-}
-//8
-$test=false;
-if (!empty($test)) {
-    echo '+';
-} else {
-    echo '-';//выведет -
-}
-//9
-$test=true;
-if (!empty($test)) {
-    echo '+';//выведет +
-} else {
-    echo '-';
-}
-//10
-$test=false;
-if (!empty($test)) {
-    echo '+';
-} else {
-    echo '-';//выведет -
-}
-//11
-$test=null;
-if (!empty($test)) {
-    echo '+';
-} else {
-    echo '-';//выведет -
-}
-//12
-$test=0;
-if (isset($test)) {
-    echo '+';//выведет +
-} else {
-    echo '-';
-}
-//13
-$test=null;
-if (isset($test)) {
-    echo '+';//выведет -
-} else {
-    echo '-';
-}
-//14
-$test=false;
-if (isset($test)) {
-    echo '+';//выведет +
-} else {
-    echo '-';
-}
+if ($test==0)
+    echo 'верно';
+else
+    echo 'неверно';
+//
+if (!$test) echo 'верно';
 ?>
 
 
