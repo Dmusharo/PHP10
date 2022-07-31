0476ee0d372dba95c1bd8de464f9d69a5e605518
Dmusharo <dmusharo@gmail.com>
2022-07-31 19:26:49 +0300

    Lesson 4. Task 34

--- index.php
+++ index.php
@@ -1,162 +1,62 @@
 <?php
-//Lesson 4. Task 33
-//   if -else булевы значение
+//Lesson 4. Task 34
+//   Команда isset               !==
+//Подавление аорнинга  isset  без ворнинга
+//инвертирование высказывания
+// Элементы массива
 //1
-$test=true;
-if ($test===true){
-    echo '+';
+$test=0;
+if (isset($test)) {
+    echo '+'; //null
 } else {
     echo '-';
 }
 //2
-$test=true;
-if ($test===false){
-    echo '+';
+$test=null;
+if (!isset($test)) {
+    echo '+'; // выведет +
 } else {
     echo '-';
 }
 //3
-$test=1;
-if ($test==true){
-    echo '+';//+
+$test=null;
+if (isset($test)) {
+    echo '+'; //
+} else {
+    echo '-';// выведет -
+}
+//4
+if (!isset($test)) {
+    echo '+'; // выведет +
 } else {
     echo '-';
 }
-//4
-$test=0;
-if ($test==false){
-    echo '+';//+
-} else {
-    echo '-'; //
-}
 //5
-$test=1;
-if ($test==true){
-    echo '+';//+
+$test='';
+if (isset($test)) {
+    echo '+'; //
 } else {
     echo '-';
 }
 //6
-$test=1;
-if ($test!=true){
-    echo '+';//
+if (!isset($test)) {
+    echo '+'; //выведет +
 } else {
-    echo '-';//-
+    echo '-';
 }
 //7
-$test='';
-if ($test==false){
-    echo '+';//
+$test=false;
+if (isset($test)) {
+    echo '+'; //выведет +
 } else {
-    echo '-';//-
+    echo '-';
 }
 //8
-$test=1;
-if ($test==true){
-    echo '+';//
-} else {
-    echo '-';//-
-}
-// Сокращенный иф
-
-//9
-$test=true;
-if ($test){
+$arr = [1, 2, 3, 4, 5];
+if (isset($arr)) {
     echo '+';
 } else {
     echo '-';
-}
-//10
-$test=true;
-if (!$test){
-    echo '+';
-} else{
-    echo '-';//-
-}
-//11
-$test=true;
-if (!$test) {
-    echo '+';
-} else {
-    echo '-'; //-
-}
-//12
-$test=true;
-if ($test) {
-    echo '+'; //+
-} else {
-    echo '-';
-}
-// Сравнеиние идет по == а не по ===      !!!!!!!!!!!
-//13
-$test=3;
-if ($test) {
-    echo '+';  //+
-} else {
-    echo '-';
-}
-//14
-$test='abc';
-if ($test) {
-    echo '+'; //+
-} else {
-    echo '-';
-}
-//15
-$test='';
-if ($test) {
-    echo '+';
-} else {
-    echo '-';//-
-}
-//16
-$test=3*'abc';
-if ($test) {
-    echo '+';
-} else {
-    echo '-'; // - нельзя умножать
-}
-//17
-$test=null;
-if ($test) {
-    echo '+';
-} else {
-    echo '-'; //-
-}
-//18
-$test=false;
-if ($test) {
-    echo '+';
-} else {
-    echo '-';
-}
-//19
-$test;   // выдает ошибку
-if ($test) {
-    echo '+';
-} else {
-    echo '-';
-}
-//20
-$test=0;
-if ($test) {
-    echo '+';
-} else {
-    echo '-';//-
-}
-//21
-$test='0';
-if ($test) {
-    echo '+';
-} else {
-    echo '-';//-
-}
-//22
-$test=-1;
-if ($test) {
-    echo '+';//+
-} else {
-    echo '-';//
 }
 ?>
 
