915ff1a1ea47ba456fc01e63e1b167ca42b31515
Dmusharo <dmusharo@gmail.com>
2022-08-01 17:20:43 +0300

    Lesson 5. Task 49

--- index.php
+++ index.php
@@ -1,26 +1,18 @@
 <?php
-//Lesson 5. Task 48
-// Цикл foreach
+//Lesson 5. Task 49
+// Подключение ключей в цикле foreach
 //1
-$arr=['a','b','c','d','e'];
-foreach ($arr as $elem){
-    echo $elem;  //Вывести все элементы массива
+$arr=['green'=>'зеленый','red'=>'Красный','blue'=>'голубой'];
+foreach ($arr as $key=>$arr['green']){
+    echo $key."\n";   ///не уверен правильно ли . Нужно проверить.
+    /// С помощью цикла foreach выведите на экран столбец ключей и элементов в формате green - зеленый.
 }
-//2 Найти сумму элементов массива
-$arr=[1,2,3,4,5,6,7,8,9];
-$sum=0;
-foreach ($arr as $elem){
-    $sum+=$elem;
+//2 С помощью цикла foreach выведите на экран столбец строк такого формата: user1 - возраст 30 лет.
+$arr = ['user1' => 30, 'user2'=> 32, 'user3' => 33];
+foreach ($arr as $user1=>$elem){
+    echo $user1.' возраст-'.$elem.' лет '."\n";
 }
-echo $sum;
-//3 Найти среднее арифметическое
-$arr=[1,2,3,4,5,6,7,8,9];
-$num=count($arr);
-$sum=0;
-foreach ($arr as $elem){
-    $sum+=$elem;
-}
-echo $sum/$num;
 ?>
+
 
 
