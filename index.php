4046802f3452dafa7307b09170a85b6e788f9e2a
Dmusharo <dmusharo@gmail.com>
2022-08-01 16:07:29 +0300

    Lesson 5. Task 46

--- index.php
+++ index.php
@@ -1,37 +1,26 @@
 <?php
-//Lesson 5. Task 45
+//Lesson 5. Task 46
 // Ошибки при работе с циклом
-//1  Вывести числа от 10 до 1
-$i=10;
-while ($i>=1){
+//1
+For ($i=1;$i<=100;$i++){
     echo $i;
-    $i--;
 }
-//2 from 10 to 1
-$i=1;
-while ($i<=10){
+//2 from 11 to 33
+for ($i=11;$i<=33;$i++){
     echo $i;
-    $i++;
 }
-//3 from 10 to 1
-$i=10;
-while ($i>0){
+//3 from 0 to 100 all even numbers
+for($i=0;$i<=100;$i+=2){
     echo $i;
-    $i--;
 }
-//4
-$i=10;
-while ($i>=0) {
+//4 from 0 to 100 all non even numbers
+for($i=1;$i<=99;$i+=2){
     echo $i;
-    $i--;
 }
-//5
-$i=10;
-while ($i>=1){
+//5 from 100 to 0 all numbers
+for ($i=100;$i>=0;$i--) {
     echo $i;
-    $i--;
 }
-
 ?>
 
 
