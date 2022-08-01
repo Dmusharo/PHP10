7e4085a450af35f8d38e7d8244f792302e068d64
Dmusharo <dmusharo@gmail.com>
2022-08-01 16:18:55 +0300

    Lesson 5. Task 47

--- index.php
+++ index.php
@@ -1,26 +1,23 @@
 <?php
-//Lesson 5. Task 46
-// Ошибки при работе с циклом
-//1
-For ($i=1;$i<=100;$i++){
-    echo $i;
+//Lesson 5. Task 47
+// Накопление результата
+$result=0;
+for ($i=1;$i<=20;$i++){
+    $result=$result+$i;
 }
-//2 from 11 to 33
-for ($i=11;$i<=33;$i++){
-    echo $i;
+echo $result; //Сумма всех чисел 1 to 20
+//2
+$result=0;
+for ($i=2;$i<=100;$i+=2){
+    $result=$result+$i;
 }
-//3 from 0 to 100 all even numbers
-for($i=0;$i<=100;$i+=2){
-    echo $i;
+echo $result; //Сумма всех чисел 2 to 100 even numbers
+//3 non even numbers from 1 to 99
+$result=1;
+for ($i=1;$i<=99;$i+=2){
+    $result=$result+$i;
 }
-//4 from 0 to 100 all non even numbers
-for($i=1;$i<=99;$i+=2){
-    echo $i;
-}
-//5 from 100 to 0 all numbers
-for ($i=100;$i>=0;$i--) {
-    echo $i;
-}
+echo $result; //Сумма всех чисел
 ?>
 
 
