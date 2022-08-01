1d35b64ae29ba8cdc32b217ac658ada84f3fa2c3
Dmusharo <dmusharo@gmail.com>
2022-08-01 13:52:34 +0300

    Lesson 4. Task 41

--- index.php
+++ index.php
@@ -1,24 +1,12 @@
 <?php
-//Lesson 4. Task 40
-// Конструкция switch-case .Выбор одного значения из ряда значений
-//1
-$num=2;
-switch($num){
-    case $num==12 or $num==1 or $num==2:
-        echo "Зима";
-        break;
-        case $num==3 or $num==4 or $num==5:
-            echo "Весна";
-            break;
-            case $num==6 or $num==7 or $num==8:
-                 echo "Лето";
-                 break;
-                 case $num==9 or $num==10 or $num==11:
-                 echo "Осень";
-                       break;
+//Lesson 4. Task 41
+// Тернарный оператор
+$num=12;
+if ($num>=0 ){
+    echo '+';
+} elseif ($num<0){
+    echo "-";
 }
-//2 default
-
 ?>
 
 
