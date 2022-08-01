7a702e2b95cd0005558f83d8e80a900be8310681
Dmusharo <dmusharo@gmail.com>
2022-08-01 13:35:25 +0300

    Lesson 4. Task 40

--- index.php
+++ index.php
@@ -1,46 +1,23 @@
 <?php
-//Lesson 4. Task 39
-// Практика условий  if-else
+//Lesson 4. Task 40
+// Конструкция switch-case .Выбор одного значения из ряда значений
 //1
-$month= 3;
-// Определить пору года.
-if ($month>=0 and $month<=2 or $month===12){
-    echo "Зима";
-} elseif ($month>=3 and $month<=5){
-    echo 'Весна';
-} elseif ($month>=6 and $month<=8) {
-    echo 'Лето';
+$num=2;
+switch($num){
+    case $num==12 or $num==1 or $num==2:
+        echo "Зима";
+        break;
+        case $num==3 or $num==4 or $num==5:
+            echo "Весна";
+            break;
+            case $num==6 or $num==7 or $num==8:
+                 echo "Лето";
+                 break;
+                 case $num==9 or $num==10 or $num==11:
+                 echo "Осень";
+                       break;
 }
-  elseif ($month>=9 and $month<=11) {
-      echo 'Осень';
-  }
-//2
-$str='abcde';
-if ($str[0]==='a'){
-    echo "Да";
-} else {
-    echo "нет";
-}
-//3
-$a=12345;
-$b=(string)$a; //!!!!!!!!!!!
-if ($b[0]==1 or $b[0]==2 or $b[0]==3){
-    echo "Да число ".$b[0];
-}
-//4
-$a=324;
-$b=(string)$a;
-//$c=(strlen($b)-1);
-echo $b[0]+$b[1]+$b[2];
-
-//5
-$a=123456;
-$b=(string)$a;
-if (($b[0]+$b[1]+$b[2])==($b[3]+$b[4]+$b[5])) {
-    echo "Yes";
-} else {
-    echo 'No';
-}
+//2 default
 
 ?>
 
