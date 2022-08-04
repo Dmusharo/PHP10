caf87707aba1db400723e7b81c42b2579f485b44
Dmusharo <dmusharo@gmail.com>
2022-08-04 13:54:56 +0300

    Lesson 5. Task 67 Заполнение массивов с накоплением строки

--- index.php
+++ index.php
@@ -1,59 +1,25 @@
 <?php
-//Lesson 5. Task 66
-//Вывод пирамидок
-//1 Вывести пирамидку с ХХ
-$str="";
-for ($i=0;$i<5;$i++){
-    $str.="xx";
-    echo $str.'<br>';
+//Lesson 5. Task 67
+// Заполнение массивов с накоплением строки
+$arr = [];
+$str = '';
+for ($i = 0; $i < 5; $i++) {
+    $str .= 'x' . 'x';
+    $arr[] = $str;
 }
 
-//2  Вывести пирамидку
-/*
- 1
-333
-55555
-7777777
-999999999
- */
-for ($i = 1; $i <= 9; $i++) {
-    $str = ''; // каждый раз зачищаем строку
+var_dump($arr); //  'xx', 'xxxx', 'xxxxxx', 'xxxxxxxx', 'xxxxxxxxxx'
 
-    for ($j = 1; $j <= $i; $j++) {
+//2 Заполнение массива числами
+$arr = [];
+for ($i = 1; $i < 6; $i++) {
+    $str = '';
+    for ($j = 0; $j < 5; $j++) {
         $str .= $i;
     }
-    if ($str%2!=0){
-        echo $str . '<br>';
-    }
-
+    $arr[] = $str;
 }
-
-//3
-/*
-xxxxxxxxxx
-xxxxxxxx
-xxxxxx
-xxxx
-xx
- */
-for ($i = 5; $i >= 1; $i--) {
-    $str = '';
-
-    for ($j = 0; $j < $i; $j++) {
-        $str .= 'xx';
-    }
-
-    echo $str . '<br>'; //Вывести пирамидку
-}
-
-//4
-for ($i = 9; $i >= 1; $i--) {
-    for ($j = 1; $j <= $i; $j++) {
-        echo $i;
-    }
-
-    echo '<br>';
-}
+var_dump($arr);
 ?>
 
 
